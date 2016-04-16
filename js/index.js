a511403db30b0152a6c4545703b6a96ea543424b
1zhao <1zhao@users.noreply.github.com>
2016-04-15 23:42:02 -0400

    Added files via upload

---
+++ js/index.js
@@ -0,0 +1,50 @@
+$(function() {
+	var message = {
+
+		message: [
+			'Geogrphy, Landsapce, Space, Place;',
+			'Urban Planning, Architecture Design, Transportation.....;',
+			'Spatial Data Visualization;',
+			'GIS Development, Remote Sensing;',
+			'ArcGIS,CartoDB,Mapbox,D3.js,....;',
+			'web development;',
+      'javascript, angular, nodejs...;',
+
+		],
+		counterS: 0,
+		counterL: 0,
+		deleteS: false,
+
+		init: function() {
+			this.cacheElem();
+			this.type();
+		},
+
+		cacheElem: function() {
+			this.$text = $('.text');
+		},
+
+		type: function() {
+			var message 	= this.message[this.counterS],
+				  that 	    = this,
+				  speed 	  = 0;
+
+			message = !this.deleteS ? message.slice(0, ++this.counterL) : message.slice(0, --this.counterL);
+			if(this.message[this.counterS] != message && !this.deleteS) {
+				this.$text.text(message);
+				speed = 90;
+			}
+			else {
+				this.deleteS = true;
+				speed = this.message[this.counterS] == message ? 1000 : 40;
+				this.$text.text(message);
+				if (message == '') {
+					this.deleteS = false;
+					this.counterS = this.counterS < this.message.length - 1 ? this.counterS + 1 : 0;
+				}
+			}
+			setTimeout(function(){that.type()}, speed);
+		}
+	};
+	message.init();
+});
